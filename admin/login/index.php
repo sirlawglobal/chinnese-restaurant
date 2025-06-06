95c58a26ed084bfbaf2c1fd08fb7499b89f53149
Charsilo162 <charlesebuka1@gmail.com>
2025-06-06 16:38:44 +0100

    another
    update

--- admin/login/index.php
+++ admin/login/index.php
@@ -9,12 +9,50 @@
     <title>Admin Sign In</title>
   </head>
   <body>
+
+
+<style>
+      .custom-alert {
+  padding: 15px 20px;
+  margin: 15px auto;
+  width: 90%;
+  max-width: 600px;
+  border-radius: 5px;
+  text-align: center;
+  font-family: Arial, sans-serif;
+  font-size: 16px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+}
+
+.custom-alert.success {
+  background-color: #d4edda;
+  color: #155724;
+  border: 1px solid #c3e6cb;
+}
+.custom-alert.error {
+    background-color: #f8d7da;
+    color: #721c24;
+    border: 1px solid #f5c6cb;
+}
+/* for error check */
+.error {
+  color: red;
+  font-size: 0.85rem;
+}
+
+</style>
+
     <main class="flex align-center justify-center wrap">
       <section class="content">
         <div class="logo">
           <img src="../../assets/images/admin-logo.png" alt="Golden Dish" />
         </div>
         <div class="head">
+          <?php if(isset($_SESSION["fail"])):?>
+          <div class="custom-alert error"><?=$_SESSION["fail"]?>
+           <?php unset($_SESSION["fail"]);
+          ?></div>
+      <?php endif?>
           <h3>Sign In</h3>
           <p>Sign in to stay connected.</p>
         </div>
